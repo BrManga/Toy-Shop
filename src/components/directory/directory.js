5fbbbcc80976a4f4f1495f662abfa4d438de0fd2
Bora Manga <bora.mng525@gmail.com>
2020-01-17 12:56:10 +0100

    State is taken into Redux

--- src/components/directory/directory.js
+++ src/components/directory/directory.js
@@ -1,60 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import MenuItem from "../menu-item/Menu-item";
 import "./directory.styles.scss";
-import babyImage from "../../assets/images/baby.jpg";
-import zweiJahre from "../../assets/images/zweiJahre.jpg";
-import fünfJahre from "../../assets/images/fünfJahre.jpg";
-import achtJahre from "../../assets/images/achtJahre.jpg";
-import zwoelfJahre from "../../assets/images/zwoelfJahre.jpg";
-
-class Directory extends Component {
-  constructor() {
-    super();
-    this.state = {
-      sections: [
-        {
-          title: "babys",
-          imageUrl: babyImage,
-          id: 1,
-          linkUrl: "shop/babys"
-        },
-        {
-          title: "2 -4 jährige kinder",
-          imageUrl: zweiJahre,
-          id: 2,
-          linkUrl: "shop/zweivier"
-        },
-        {
-          title: "5 - 7 jährige kinder",
-          imageUrl: fünfJahre,
-          id: 3,
-          linkUrl: "shop/fünfsieben"
-        },
-        {
-          title: "8 - 11 jährige kinder",
-          imageUrl: achtJahre,
-          size: "large",
-          id: 4,
-          linkUrl: "shop/achtelf"
-        },
-        {
-          title: "12 & Up",
-          imageUrl: zwoelfJahre,
-          size: "large",
-          id: 5,
-          linkUrl: "shop/zwoelfup"
-        }
-      ]
-    };
-  }
-  render() {
-    return (
-      <div className="directory-menu">
-        {this.state.sections.map(({ id, ...otherSectionProps }) => (
+import {connect} from "react-redux"
+import {createStructuredSelector} from "reselect"
+import {selectDirectorySections} from "../../redux/directory/directory.selectors"
+const Directory=({sections})=> ( 
+      (<div className="directory-menu">
+        {sections.map(({ id, ...otherSectionProps }) => (
           <MenuItem key={id} {...otherSectionProps} />
         ))}
-      </div>
-    );
-  }
-}
-export default Directory;
+      </div>))
+    
+
+const mapStateToProps=createStructuredSelector({
+  sections:selectDirectorySections
+})
+export default connect(mapStateToProps)(Directory);
---
+++ src/redux/directory/directory.reducer.js
@@ -0,0 +1,49 @@
+import babyImage from "../../assets/images/baby.jpg";
+import zweiJahre from "../../assets/images/zweiJahre.jpg";
+import fünfJahre from "../../assets/images/fünfJahre.jpg";
+import achtJahre from "../../assets/images/achtJahre.jpg";
+import zwoelfJahre from "../../assets/images/zwoelfJahre.jpg";
+let initialState = {
+  sections: [
+    {
+      title: "babys",
+      imageUrl: babyImage,
+      id: 1,
+      linkUrl: "shop/babys"
+    },
+    {
+      title: "2 -4 jährige kinder",
+      imageUrl: zweiJahre,
+      id: 2,
+      linkUrl: "shop/zweivier"
+    },
+    {
+      title: "5 - 7 jährige kinder",
+      imageUrl: fünfJahre,
+      id: 3,
+      linkUrl: "shop/fünfsieben"
+    },
+    {
+      title: "8 - 11 jährige kinder",
+      imageUrl: achtJahre,
+      size: "large",
+      id: 4,
+      linkUrl: "shop/achtelf"
+    },
+    {
+      title: "12 & Up",
+      imageUrl: zwoelfJahre,
+      size: "large",
+      id: 5,
+      linkUrl: "shop/zwoelfup"
+    }
+  ]
+};
+
+const directoryReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default directoryReducer;
