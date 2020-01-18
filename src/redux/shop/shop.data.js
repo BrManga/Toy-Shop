c23a9b35ecb509fce6e203a771593fb307310344
Bora Manga <bora.mng525@gmail.com>
2020-01-18 12:50:30 +0100

    directory and shop data moved to reducers

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,213 +1,256 @@
-const SHOP_DATA = [
-  {
+const SHOP_DATA =  {
+  babys:{
     id: 1,
-    title: 'Babys',
-    routeName: 'babys',
+    title: "Babys",
+    routeName: "babys",
     items: [
       {
         id: 1,
-        name: 'Baby Toy',
-        imageUrl: "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Baby Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 25
       },
       {
         id: 2,
-        name: 'Baby Toy',
-         imageUrl: 'https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        name: "Baby Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 18
       },
       {
         id: 3,
-        name: 'Baby Toy',
-        imageUrl: 'https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        name: "Baby Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 35
       },
       {
         id: 4,
-        name: 'Baby Toy',
-        imageUrl: 'https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+        name: "Baby Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        price: 25
+      },
+      {
+        id: 5,
+        name: "Baby Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        price: 25
+      },
+      {
+        id: 6,
+        name: "Baby Toys",
+        imageUrl:
+          "https://images.unsplash.com/photo-1511948374796-056e8f289f34?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 25
       }
     ]
   },
-  {
+  zweivierjaehrige:{
     id: 2,
-    title: '2 -4 jährige kinder',
-    routeName: 'zweivierjaehrige',
+    title: "2 -4 jährige kinder",
+    routeName: "zweivierjaehrige",
     items: [
       {
         id: 10,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 11,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 12,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 13,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 14,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 15,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       },
       {
         id: 16,
-        name: '2 -4 jährige Toy',
-        imageUrl: "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80", 
+        name: "2 -4 jährige Toy",
+        imageUrl:
+          "https://images.unsplash.com/photo-1545558014-8692077e9b5c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 220
       }
     ]
   },
-  {
+  fuenfsiebenjaehrige:{
     id: 3,
-    title: '5 - 7 jährige kinder',
-    routeName: 'fuenfsiebenjaehrige',
+    title: "5 - 7 jährige kinder",
+    routeName: "fuenfsiebenjaehrige",
     items: [
       {
         id: 18,
-        name: 'Lego',
-        imageUrl: "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        name: "Lego",
+        imageUrl:
+          "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         price: 125
       },
       {
         id: 19,
-        name: 'Lego',
-        imageUrl: "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        name: "Lego",
+        imageUrl:
+          "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         price: 125
       },
       {
         id: 20,
-        name: 'Lego',
-        imageUrl: "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        name: "Lego",
+        imageUrl:
+          "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         price: 125
       },
       {
         id: 21,
-        name: 'Lego',
-        imageUrl: "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        name: "Lego",
+        imageUrl:
+          "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         price: 125
       },
       {
         id: 22,
-        name: 'Lego',
-        imageUrl: "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
+        name: "Lego",
+        imageUrl:
+          "https://images.unsplash.com/photo-1516981879613-9f5da904015f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=667&q=80",
         price: 125
       }
     ]
   },
-  {
+  achtelfjaehrige:{
     id: 4,
-    title: '8 - 11 jährige kinder',
-    routeName: 'achtelfjaehrige',
+    title: "8 - 11 jährige kinder",
+    routeName: "achtelfjaehrige",
     items: [
       {
         id: 23,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 24,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 25,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 26,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 27,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 28,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       },
       {
         id: 29,
-        name: 'Robot',
-        imageUrl: "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
+        name: "Robot",
+        imageUrl:
+          "https://images.unsplash.com/photo-1549512329-31d4e8831543?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=375&q=80",
         price: 25
       }
     ]
   },
-  {
+  zwoelfup:{
     id: 5,
-    title: '12 & up ',
-    routeName: 'zwoelfup',
+    title: "12 & up ",
+    routeName: "zwoelfup",
     items: [
       {
         id: 30,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       },
       {
         id: 31,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       },
       {
         id: 32,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       },
       {
         id: 33,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       },
       {
         id: 34,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       },
       {
         id: 35,
-        name: 'Playstation',
-        imageUrl: "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+        name: "Playstation",
+        imageUrl:
+          "https://images.unsplash.com/photo-1493711662062-fa541adb3fc8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
         price: 325
       }
     ]
   }
-];
+};
 
 export default SHOP_DATA;
